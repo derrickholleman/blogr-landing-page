8c57f9ec3c23c7eac941e9646f00622812e842fe
Derrick Holleman <derrickholleman@gmail.com>
2021-05-03 14:39:45 -0500

    deleted hamburger id, made it strictly a class

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,24 +1,24 @@
 /* hamburger menu animation toggle */
 $(document).ready(function(){
-	$('#hamburger').click(function(){
+	$('.hamburger').click(function(){
 		$(this).toggleClass('open');
 	});
 });
 
 /* hamburger menu open */
-const menu_toggle = document.querySelector('#hamburger');
+const menu_toggle = document.querySelector('.hamburger');
 const mobile_menu_display = document.querySelector('.mobile-nav');
 const hide_header = document.querySelector('.header-bottom-row')
 
-menu_toggle.addEventListener('click', function () { /* if #hamburger is clicked */
+menu_toggle.addEventListener('click', function () {
 	mobile_menu_display.classList.toggle('is-active'); /* init .mobile-nav.is-active */
     hide_header.classList.toggle('is-active'); /* init .header-bottom-row.is-active */
-    menu_toggle.classList.toggle('is-active'); /* only need this for the jQuery code that tells the body not to scroll while menu is open */
+    menu_toggle.classList.toggle('is-active'); /* init .is-active on .hamburger */
 })
 /*----------------------*/
 
 /* prevents the hamburger menu from scrolling while open */
-jQuery('#hamburger').click(function(){
+jQuery('.hamburger').click(function(){
     var menu_active = jQuery(this).hasClass('is-active');
     if(menu_active){
         jQuery('body').css('position','fixed');
@@ -29,7 +29,7 @@ jQuery('#hamburger').click(function(){
 });
 
 /* ALTERNATIVE WAY TO HIDE TEXT BASED ON A CONDITION */
-// jQuery('#hamburger').click(function(){
+// jQuery('.hamburger').click(function(){
 //     var menu_active = jQuery(this).hasClass('is-active');
 //     if(menu_active){
 //         jQuery('.header-bottom-row').css('display', 'none');
