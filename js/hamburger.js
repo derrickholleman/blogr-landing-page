01121220fb10ed5be8f9d44d0db13bd5c76d1453
Derrick Holleman <derrickholleman@gmail.com>
2021-05-02 21:33:50 -0500

    more clarity in hamburger.js

--- js/hamburger.js
+++ js/hamburger.js
@@ -8,13 +8,15 @@ $(document).ready(function(){
 /* hamburger menu open */
 /* const menu_btn toggles on whichever classes have the .is-active class */
 /* const mobile_menu toggles on .is-active onto .mobile-nav */
-const menu_btn = document.querySelector('#hamburger');
+const menu_toggle = document.querySelector('#hamburger');
 const mobile_menu = document.querySelector('.mobile-nav');
 
-menu_btn.addEventListener('click', function () {
-	menu_btn.classList.toggle('is-active');
+/* if clicked, #hamburger activates the .is-active class, then toggles it on for .mobile-nav */
+menu_toggle.addEventListener('click', function () {
+	menu_toggle.classList.toggle('is-active');
 	mobile_menu.classList.toggle('is-active');
 })
+/*----------------------*/
 
 /* prevents the hamburger menu from scrolling while open */
 jQuery('#hamburger').click(function(){
